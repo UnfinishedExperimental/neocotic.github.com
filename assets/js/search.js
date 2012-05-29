a0a64680b291b24d40f991c046d22c267032749a
Alasdair Mercer <mercer.alasdair@gmail.com>
2012-05-29 14:05:41 +0000

    improved search page

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,27 +1,74 @@
-function parseQueryFromUrl() {
-  var queryParamName = 'q'
-    , search = window.location.search.substr(1)
-    , parts = search.split('&');
-  for (var i = 0; i < parts.length; i++) {
-    var keyvaluepair = parts[i].split('=');
-    if (decodeURIComponent(keyvaluepair[0]) === queryParamName) {
-      return decodeURIComponent(keyvaluepair[1].replace(/\+/g, ' '));
+(function() {
+
+  var QUERY_PARAM = 'q';
+
+  function parseParamsFromUrl() {
+    var search = window.location.search.substr(1)
+      , parts = search.split('&');
+    for (var i = 0; i < parts.length; i++) {
+      var keyvaluepair = parts[i].split('=');
+      if (decodeURIComponent(keyvaluepair[0]) === QUERY_PARAM) {
+        return decodeURIComponent(keyvaluepair[1].replace(/\+/g, ' '));
+      }
     }
+    return '';
   }
-  return '';
-}
 
-google.load('search', '1', {language : 'en'});
+  var customSearchControl
+    , queryFromUrl = parseParamsFromUrl();
 
-google.setOnLoadCallback(function() {
-  var customSearchControl = new google.search.CustomSearchControl(
-    '010241032241406631661:WMX2654048');
-  customSearchControl.setResultSetSize(
-    google.search.Search.LARGE_RESULTSET);
-  var options = new google.search.DrawOptions();
-  options.setAutoComplete(true);
-  options.enableSearchResultsOnly();
-  customSearchControl.draw('cse', options);
-  var queryFromUrl = parseQueryFromUrl();
-  if (queryFromUrl) customSearchControl.execute(queryFromUrl);
-}, true);
+  google.load('search', '1', {language : 'en'});
+
+  google.setOnLoadCallback(function() {
+    customSearchControl = new google.search.CustomSearchControl(
+      '010241032241406631661:WMX2654048');
+    customSearchControl.setResultSetSize(
+      google.search.Search.LARGE_RESULTSET);
+    var options = new google.search.DrawOptions();
+    options.setAutoComplete(true);
+    options.enableSearchResultsOnly();
+    customSearchControl.draw('cse', options);
+    if (queryFromUrl) customSearchControl.execute(queryFromUrl);
+  }, true);
+
+  $(function() {
+
+    var form = $('#cse-input')
+      , input = form.find('input[name="' + QUERY_PARAM + '"]').val(
+          queryFromUrl)
+      , loaded = false;
+
+    if (queryFromUrl) $('#cse > .progress').show();
+
+    $(window).on('popstate', function() {
+      queryFromUrl = parseParamsFromUrl();
+      input.val(queryFromUrl);
+      if (!loaded) {
+        loaded = true;
+      } else if (customSearchControl) {
+        if (queryFromUrl) {
+          customSearchControl.execute(queryFromUrl);
+        } else {
+          customSearchControl.clearAllResults();
+        }
+      }
+    });
+
+    form.on('submit', function() {
+      if (customSearchControl) {
+        var queryFromInput = input.val();
+        if (queryFromInput) {
+          customSearchControl.execute(queryFromInput);
+        } else {
+          customSearchControl.clearAllResults();
+        }
+        if ($.isFunction(window.history.pushState)) {
+          window.history.pushState({}, null, '?q=' + queryFromInput);
+        }
+        return false;
+      }
+    });
+
+  });
+
+}());
